47cb9ef073661e72928e8699fb795a6ebfbbed51
Anshul Kumar <anshulk0102@gmail.com>
2026-01-30 08:40:57 +0000

    entity added

---
+++ src/main/java/org/example/weather/model/WeatherEntity.java
@@ -0,0 +1,56 @@
+package org.example.weather.model;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.util.List;
+@Entity
+@Table(name="favorite_cities")
+public class WeatherEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment sql thing
+    public Long id;
+    @Column(nullable=false)
+    public String city;
+    @Column
+    public double temperature;
+    @Column
+    public boolean alert;
+    @Column
+    public Timestamp last_updated;
+
+    public WeatherEntity(long id, String city, boolean alert, Timestamp last_updated, double temperature) {
+        this.id = id;
+        this.city = city;
+        this.alert = alert;
+        this.last_updated = last_updated;
+        this.temperature=temperature;
+
+    }
+
+    public WeatherEntity() {
+    }
+
+    public String getCity() {
+        return city;
+    }
+    public void setCityAsFavorite(String city) {
+        this.city = city;}
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setAlert(boolean alert) {
+        this.alert=alert;
+    }
+    public boolean getAlert() {
+        return alert;
+    }
+}
+
